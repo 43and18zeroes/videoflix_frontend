4a3f402926ffb067329aa7f6abc0a9ec95892e6d
Christoph <c.wagner.germany@gmail.com>
2025-03-15 17:09:39 +0100

    iphonefooter

--- src/app/auth/services/device/device.service.ts
+++ src/app/auth/services/device/device.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
+import { Injectable, Renderer2, RendererFactory2, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -6,20 +7,35 @@ import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
 export class DeviceService {
   private renderer: Renderer2;
   isAndroid: boolean = false;
+  isiPhone: boolean = false;
 
-  constructor(rendererFactory: RendererFactory2) {
+  constructor(
+    rendererFactory: RendererFactory2,
+    @Inject(PLATFORM_ID) private platformId: Object // Überprüft, ob es der Browser ist
+  ) {
     this.renderer = rendererFactory.createRenderer(null, null);
-    this.isAndroid = /Android/i.test(navigator.userAgent);
 
-    if (this.isAndroid) {
-      this.applyAndroidClassToFooter();
+    // Prüfen, ob der Code im Browser läuft
+    if (isPlatformBrowser(this.platformId)) {
+      this.isAndroid = /Android/i.test(navigator.userAgent);
+      this.isiPhone = /iPhone|iPod/i.test(navigator.userAgent);
+
+      this.applyDeviceClassToFooter();
     }
   }
 
-  private applyAndroidClassToFooter(): void {
-    const footer = document.querySelector('footer');
-    if (footer) {
-      this.renderer.addClass(footer, 'android__footer');
+  private applyDeviceClassToFooter(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const footer = document.querySelector('footer');
+
+      if (footer) {
+        if (this.isAndroid) {
+          this.renderer.addClass(footer, 'android__footer');
+        } 
+        if (this.isiPhone) {
+          this.renderer.addClass(footer, 'iphone__footer');
+        }
+      }
     }
   }
 }
