27b0392e6ce48ad39b0008578d6d5140e57c63a7
Christoph <c.wagner.germany@gmail.com>
2025-03-16 15:41:14 +0100

    email

--- src/app/auth/start/start.component.ts
+++ src/app/auth/start/start.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-start',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './start.component.html',
-  styleUrl: './start.component.scss'
+  styleUrl: './start.component.scss',
 })
 export class StartComponent {
+  email: string = '';
 
+  submitEmail() {
+    console.log('E-Mail-Adresse:', this.email);
+  }
 }
