863fd7acd7312ab60be02a1ebd1df1fcfe6b8175
Christoph <c.wagner.germany@gmail.com>
2025-03-19 17:19:18 +0100

    auth-footer working

--- src/app/auth/start/start.component.ts
+++ src/app/auth/start/start.component.ts
@@ -3,11 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../services/auth.service';
 import { AuthHeaderComponent } from '../components/auth-header/auth-header.component';
+import { AuthFooterComponent } from '../components/auth-footer/auth-footer.component';
 
 @Component({
   selector: 'app-start',
   standalone: true,
-  imports: [CommonModule, FormsModule, AuthHeaderComponent],
+  imports: [CommonModule, FormsModule, AuthHeaderComponent, AuthFooterComponent],
   templateUrl: './start.component.html',
   styleUrl: './start.component.scss',
 })
