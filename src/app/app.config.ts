d240350c82d32942a6a6e21120b4e844eb252a67
Christoph <c.wagner.germany@gmail.com>
2025-03-19 17:28:52 +0100

    , withFetch

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,13 @@ import { provideRouter, Routes } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { authRoutes } from './auth/auth-routing.module';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter([...routes, ...authRoutes]),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
   ]
 };
