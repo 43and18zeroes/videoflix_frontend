4b3ed11987c478db96bd2545c76b2c294b21c715
Christoph <c.wagner.germany@gmail.com>
2025-03-19 17:36:54 +0100

    signup

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-
+import { AuthHeaderComponent } from '../components/auth-header/auth-header.component';
+import { AuthFooterComponent } from '../components/auth-footer/auth-footer.component';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [AuthHeaderComponent, AuthFooterComponent, FormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+  email: string = '';
+
+  submitPasswd() {}
 
 }
