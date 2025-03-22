8843a0dcae191dd338b062b6074cb2706af22746
Christoph <c.wagner.germany@gmail.com>
2025-03-22 20:54:50 +0100

    backgrounds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DeviceService } from './services/device/device.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  constructor(private deviceService: DeviceService) {}
   title = 'videoflix_frontend';
+  backgroundClass = '';
+
+  constructor(private router: Router, private deviceService: DeviceService) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.updateBackgroundClass(event.url);
+      }
+    });
+  }
+
+  updateBackgroundClass(url: string) {
+    if (url === '/') {
+      this.backgroundClass = 'start-background';
+    } else if (url.startsWith('/signup') || url.startsWith('/forgot-password') || url.startsWith('/login') || url.startsWith('/password-reset')) {
+      this.backgroundClass = 'auth-background';
+    } else {
+      this.backgroundClass = '';
+    }
+  }
 }
