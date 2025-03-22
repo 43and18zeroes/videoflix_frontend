bd907dd308fb30cdac1313b2f56e32032a006aa2
Christoph <c.wagner.germany@gmail.com>
2025-03-22 20:13:45 +0100

    authguard_working

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,13 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { authRoutes } from './auth/auth-routing.module';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { VideoOfferComponent } from './protected/video-offer/video-offer.component';
+import { AuthGuard } from './protected/auth.guard';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter([...routes, ...authRoutes]),
+    provideRouter([...routes, ...authRoutes, { path: 'video-offer', component: VideoOfferComponent, canActivate: [AuthGuard] }]),
     provideHttpClient(withFetch()),
   ]
 };
