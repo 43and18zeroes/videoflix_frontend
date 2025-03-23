db2b045a4caefa7de38dc920fd3a4f6764b7302c
Christoph <c.wagner.germany@gmail.com>
2025-03-23 15:33:38 +0100

    authstyles

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { AuthFooterComponent } from '../components/auth-footer/auth-footer.component';
+import { AuthHeaderComponent } from '../components/auth-header/auth-header.component';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
+  imports: [AuthHeaderComponent, AuthFooterComponent],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.scss'
 })
--- src/app/auth/password-reset/password-reset.component.ts
+++ src/app/auth/password-reset/password-reset.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { AuthFooterComponent } from '../components/auth-footer/auth-footer.component';
+import { AuthHeaderComponent } from '../components/auth-header/auth-header.component';
 
 @Component({
   selector: 'app-password-reset',
-  imports: [],
+  imports: [AuthHeaderComponent, AuthFooterComponent],
   templateUrl: './password-reset.component.html',
   styleUrl: './password-reset.component.scss'
 })
