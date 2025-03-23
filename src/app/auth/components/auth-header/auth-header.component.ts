02b43a718f433254e59e22534ba57b8d9910a537
Christoph <c.wagner.germany@gmail.com>
2025-03-23 15:30:31 +0100

    updateLoginButtonVisibility

--- src/app/auth/components/auth-header/auth-header.component.ts
+++ src/app/auth/components/auth-header/auth-header.component.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-auth-header',
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './auth-header.component.html',
-  styleUrl: './auth-header.component.scss'
+  styleUrl: './auth-header.component.scss',
 })
 export class AuthHeaderComponent {
+  showLoginButton: boolean = false;
 
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.updateLoginButtonVisibility(event.url);
+      }
+    });
+  }
+
+  updateLoginButtonVisibility(url: string) {
+    this.showLoginButton = url === '/';
+  }
+
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { AuthHeaderComponent } from '../components/auth-header/auth-header.component';
+import { AuthFooterComponent } from '../components/auth-footer/auth-footer.component';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [AuthHeaderComponent, AuthFooterComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
