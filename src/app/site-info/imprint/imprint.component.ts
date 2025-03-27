56d26ee87f06312ee0e20b63c2634de130d5cd51
Christoph <c.wagner.germany@gmail.com>
2025-03-27 16:50:43 +0100

    siteinfo2

--- src/app/site-info/imprint/imprint.component.ts
+++ src/app/site-info/imprint/imprint.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { AuthHeaderComponent } from '../../auth/components/auth-header/auth-header.component';
+import { AuthFooterComponent } from '../../auth/components/auth-footer/auth-footer.component';
 
 @Component({
   selector: 'app-imprint',
-  imports: [],
+  imports: [AuthHeaderComponent, AuthFooterComponent],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
--- src/app/site-info/privacy-policy/privacy-policy.component.ts
+++ src/app/site-info/privacy-policy/privacy-policy.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { AuthHeaderComponent } from '../../auth/components/auth-header/auth-header.component';
+import { AuthFooterComponent } from '../../auth/components/auth-footer/auth-footer.component';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [],
+  imports: [AuthHeaderComponent, AuthFooterComponent],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
