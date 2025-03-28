ff6a92c46d300667500878286bd9fc24bd09075c
Christoph <c.wagner.germany@gmail.com>
2025-03-28 18:36:12 +0100

    mail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrivacyPolicyComponent } from './auth/site-info/privacy-policy/privacy-policy.component';
 import { ImprintComponent } from './auth/site-info/imprint/imprint.component';
+import { ConfirmEmailComponent } from './auth/mails/confirm-email/confirm-email.component';
 
 export const routes: Routes = [
   { path: 'privacy-policy', component: PrivacyPolicyComponent },
   { path: 'imprint', component: ImprintComponent },
+  { path: 'confirm-email-edit', component: ConfirmEmailComponent },
 ];
 
 @NgModule({
