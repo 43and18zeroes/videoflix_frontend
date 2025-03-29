1a039a115df865a359062a68f949ef777b21e8f5
Christoph <c.wagner.germany@gmail.com>
2025-03-29 16:18:21 +0100

    removed validation

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -2,56 +2,64 @@ import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { AuthHeaderComponent } from '../components/auth-header/auth-header.component';
 import { AuthFooterComponent } from '../components/auth-footer/auth-footer.component';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [AuthHeaderComponent, AuthFooterComponent, FormsModule, ReactiveFormsModule, CommonModule],
+  imports: [
+    AuthHeaderComponent,
+    AuthFooterComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.scss'
+  styleUrl: './sign-up.component.scss',
 })
 export class SignUpComponent {
   token = '';
   setPasswordForm: FormGroup;
 
-  constructor(private route: ActivatedRoute, private authService: AuthService, private fb: FormBuilder, private router: Router) {
-    this.setPasswordForm = this.fb.group({
-      password: ['', [Validators.required, Validators.minLength(8)]],
-      confirmPassword: ['', Validators.required]
-    }, { validators: this.passwordMatchValidator });
+  constructor(
+    private route: ActivatedRoute,
+    private authService: AuthService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+    this.setPasswordForm = this.fb.group(
+      {
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required],
+      }
+    );
   }
 
   ngOnInit() {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       this.token = params['token'];
     });
-  }
-
-  passwordMatchValidator(formGroup: FormGroup) {
-    const password = formGroup.get('password')?.value;
-    const confirmPassword = formGroup.get('confirmPassword')?.value;
-
-    if (password !== confirmPassword) {
-      formGroup.get('confirmPassword')?.setErrors({ passwordMismatch: true });
-    } else {
-      formGroup.get('confirmPassword')?.setErrors(null);
-    }
   }
 
   onSubmit() {
     if (this.setPasswordForm.valid) {
       const password = this.setPasswordForm.value.password;
       this.authService.setPassword(this.token, password).subscribe(
-        response => {
+        (response) => {
           this.router.navigate(['/login']);
           localStorage.setItem('access_token', response.access);
           localStorage.setItem('refresh_token', response.refresh);
           this.router.navigate(['/video-offer']);
         },
-        error => {
-          console.log("Password error")
+        (error) => {
+          console.log('Password error');
         }
       );
     }
