cfdfca4b40d6d9221d5bbef24eed5fa5389357ff
Christoph <c.wagner.germany@gmail.com>
2025-04-01 21:29:28 +0200

    logout button

--- src/app/protected/video-offer/video-offer.component.ts
+++ src/app/protected/video-offer/video-offer.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-video-offer',
   imports: [],
   templateUrl: './video-offer.component.html',
-  styleUrl: './video-offer.component.scss'
+  styleUrl: './video-offer.component.scss',
 })
 export class VideoOfferComponent {
+  constructor(private router: Router) {}
 
+  logout(): void {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+    this.router.navigate(['/login']); // Weiterleitung zur Login-Seite
+  }
 }
