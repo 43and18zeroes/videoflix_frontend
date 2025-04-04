1dc7b6aae388a6793df1c77ee950602abce02a56
Christoph <c.wagner.germany@gmail.com>
2025-04-04 12:31:11 +0200

    pupupstart

--- src/app/auth/start/start.component.ts
+++ src/app/auth/start/start.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-
 import { AuthService } from '../services/auth.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AuthHeaderComponent } from '../components/auth-header/auth-header.component';
 import { AuthFooterComponent } from '../components/auth-footer/auth-footer.component';
 import { LoadingOverlayComponent } from '../../global-components/loading-overlay/loading-overlay.component';
-
+import { SuccessPopupComponent } from '../../global-components/success-popup/success-popup.component';
 
 @Component({
   selector: 'app-start',
@@ -16,7 +15,8 @@ import { LoadingOverlayComponent } from '../../global-components/loading-overlay
     FormsModule,
     AuthHeaderComponent,
     AuthFooterComponent,
-    LoadingOverlayComponent
+    LoadingOverlayComponent,
+    SuccessPopupComponent,
   ],
   templateUrl: './start.component.html',
   styleUrl: './start.component.scss',
@@ -27,17 +27,17 @@ export class StartComponent {
   loading = false;
   showPopup = false;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
   submitEmail() {
     this.loading = true;
     this.authService.registerUser(this.email).subscribe(
-      response => {
+      (response) => {
         console.log('Registration successful', response);
         this.loading = false;
         this.showPopup = true;
       },
-      error => {
+      (error) => {
         console.error('Registration failed', error);
         this.loading = false;
       }
