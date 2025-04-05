7e922a1eaa5ee8d6ddbbca57784e698918c33d80
Christoph <c.wagner.germany@gmail.com>
2025-04-05 14:39:50 +0200

    endpoint2

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
