e08c49cd5056f8ee95968ce5ba03b1dcd87fd7e0
Christoph <c.wagner.germany@gmail.com>
2025-04-11 17:01:37 +0200

    NgFor

--- src/app/protected/video-offer/video-offer.component.ts
+++ src/app/protected/video-offer/video-offer.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { ProtectedHeaderComponent } from '../components/protected-header/protected-header.component';
+import { NgFor } from '@angular/common';
+
+interface VideoSection {
+  title: string;
+  thumbnails: string[];
+}
 
 @Component({
   selector: 'app-video-offer',
   standalone: true,
-  imports: [ProtectedHeaderComponent],
+  imports: [ProtectedHeaderComponent, NgFor],
   templateUrl: './video-offer.component.html',
   styleUrl: './video-offer.component.scss',
 })
 export class VideoOfferComponent {
+
+  sections: VideoSection[] = [
+    {
+      title: 'New on Videoflix',
+      thumbnails: [
+        'assets/img/video-offer/videos_thumbnails/bird-324.webp',
+        'assets/img/video-offer/videos_thumbnails/blue-sky-324.webp',
+      ],
+    },
+    {
+      title: 'Documentary',
+      thumbnails: [
+        'assets/img/video-offer/videos_thumbnails/boats-324.webp',
+        'assets/img/video-offer/videos_thumbnails/campfire-324.webp',
+      ],
+    },
+  ];
+
+
   playVideo() {}
 }
