592e8743bc3fdcfcd24bbbbdd39d78e14ae4f18c
Christoph <c.wagner.germany@gmail.com>
2025-04-11 17:27:42 +0200

    additionalvids

--- src/app/protected/video-offer/video-offer.component.ts
+++ src/app/protected/video-offer/video-offer.component.ts
@@ -28,15 +28,19 @@ export class VideoOfferComponent {
     {
       title: 'New on Videoflix',
       thumbnails: [
-        { thumbnailUrl: `${this.thumbnailBasePath}bird-324.webp`, videoId: 'bird-video-001', altText: 'Thumbnail of bird video' },
-        { thumbnailUrl: `${this.thumbnailBasePath}blue-sky-324.webp`, videoId: 'sky-video-456', altText: 'Thumbnail of sky video' }
+        { thumbnailUrl: this.thumbnailBasePath + 'bird-324.webp', videoId: 'bird-video-001', altText: 'Thumbnail of bird video' },
+        { thumbnailUrl: this.thumbnailBasePath + 'blue-sky-324.webp', videoId: 'sky-video-001', altText: 'Thumbnail of sky video' },
+        { thumbnailUrl: this.thumbnailBasePath + 'boats-324.webp', videoId: 'boats-video-001', altText: 'Thumbnail of boats video' },
+        { thumbnailUrl: this.thumbnailBasePath + 'campfire-324.webp', videoId: 'campfire-video-001', altText: 'Thumbnail of campfire video' },
+        { thumbnailUrl: this.thumbnailBasePath + 'cows-324.webp', videoId: 'cows-video-001', altText: 'Thumbnail of cows video' },
+        { thumbnailUrl: this.thumbnailBasePath + 'goat-324.webp', videoId: 'goat-video-001', altText: 'Thumbnail of goat video' },
       ],
     },
     {
       title: 'Documentary',
       thumbnails: [
-        { thumbnailUrl: this.thumbnailBasePath + 'boats-324.webp', videoId: 'boats-doc-789' },
-        { thumbnailUrl: this.thumbnailBasePath + 'campfire-324.webp', videoId: 'campfire-doc-101' }
+        { thumbnailUrl: this.thumbnailBasePath + 'gras-324.webp', videoId: 'gras-video-001', altText: 'Thumbnail of gras video' },
+        { thumbnailUrl: this.thumbnailBasePath + 'mountain-324.webp', videoId: 'mountain-video-001', altText: 'Thumbnail of mountain video' },
       ],
     },
   ];
